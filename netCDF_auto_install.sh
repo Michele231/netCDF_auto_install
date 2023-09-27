700e502d04d2961eb5d0db996aa1d71d283d3354
Michele231 <michele.martinazzo2@unibo.it>
2023-09-27 11:52:47 +0200

    v0.1

---
+++ netCDF_auto_install.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+#
+# Code created to perform an automatic installation of the
+# C and FORTRAN netCDF4 libraries.
+#
+# OS    : Ubuntu 22.04 LTS
+# Author: Michele Martinazzo
+# Usage : ./netCDF_auto_install.sh
+#
+
+# You may need to install also "libxml2-dev" and "libcurl4-openssl-dev": 
+#                              >> sudo apt install libxml2-dev
+#                              >> sudo apt install libcurl4-openssl-dev
+
+# If you need to install netCDF4 for python you can export the following 
+# envirorment variables (ifolder_ are defined below):
+# export HDF5_DIR=ifolder_hdf5
+# export NETCDF4_DIR=ifolder_ncC
+# Then run: 
+#          >>sudo -E pip install netCDF4 --upgrade
+
+# zlib version (1.2.8) 
+zlinv=1.2.13
+# hdf5 version (1.8.13) 
+hdf5v=1.12.1
+# netcdf-c- version (4.7.4) 
+ncCv=4.9.2
+# netcdf-FORTRAN version (4.5.4)
+ncFv=4.5.4
+# Installation folder
+ifolder_path=/usr/local
+
+
+og_path=$(pwd)
+
+# Creation log file
+if [ -f installation_log.txt ]; then
+  rm installation_log.txt
+  touch installation_log.txt
+else
+  touch installation_log.txt
+fi
+
+# Creation of the installation folder
+ifolder=$ifolder_path/netCDF_auto_install
+ifolder_zlib=$ifolder/zlib
+=$ifolder/hdf5
+ifolder_ncC=$ifolder/netcdf4_c
+ifolder_ncF=$ifolder/netcdf4_f
+
+if [ ! -d "$ifolder" ]; then
+  echo "Creation of the installation directory in $ifolder."
+  echo "Creation of the installation directory in $ifolder." >> installation_log.txt
+  mkdir $ifolder
+  mkdir $ifolder_zlib
+  mkdir $ifolder_hdf5
+  mkdir $ifolder_ncC
+  mkdir $ifolder_ncF
+else
+  echo "The directory $ifolder already exist!"
+  exit
+fi
+
+
+################# zlib #################
+
+echo "Downloading zlib version ${zlinv}"
+echo "Downloading zlib version ${zlinv}" >> installation_log.txt
+wget https://zlib.net/fossils/zlib-${zlinv}.tar.gz
+tar xvzf zlib-${zlinv}.tar.gz >/dev/null
+cd zlib-${zlinv}
+
+echo "Installing zlib version ${zlinv}"
+echo "Installing zlib version ${zlinv}" >> installation_log.txt
+
+./configure --prefix=$ifolder_zlib #1>/dev/null
+make #1>/dev/null
+make check #1>/dev/null
+sudo make install
+
+cd $og_path
+
+################# hdf5 #################
+
+echo "Downloading hdf5 version ${hdf5v}"
+echo "Downloading hdf5 version ${hdf5v}" >> installation_log.txt
+hdf5vv=$(echo $hdf5v | cut -d '.' -f 1,2)
+wget --content-disposition --no-check-certificate https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-${hdf5vv}/hdf5-${hdf5v}/src/hdf5-${hdf5v}.tar.bz2
+tar xvf hdf5-${hdf5v}.tar.bz2 >/dev/null
+cd hdf5-${hdf5v}
+
+echo "Installing hdf5 version ${hdf5v}"
+echo "Installing hdf5 version ${hdf5v}" >> installation_log.txt
+
+./configure --enable-shared --enable-hl --with-zlib=$ifolder_zlib --prefix=$ifolder_hdf5 #1>/dev/null
+make #1>/dev/null
+make check #1>/dev/null
+sudo make install
+cd $og_path
+
+
+################# netcdf-C #################
+
+echo "Downloading netcdf-C version ${ncCv}"
+echo "Downloading netcdf-C version ${ncCv}" >> installation_log.txt
+wget https://github.com/Unidata/netcdf-c/archive/v${ncCv}.tar.gz
+mv v${ncCv}.tar.gz ncC_v${ncCv}.tar.gz
+tar xvzf ncC_v${ncCv}.tar.gz >/dev/null
+
+cd netcdf-c-${ncCv}
+
+echo "Installing netcdf-c version ${ncCv}"
+echo "Installing netcdf-c version ${ncCv}" >> installation_log.txt
+
+CPPFLAGS=-I$ifolder_hdf5/include LDFLAGS=-L$ifolder_hdf5/lib ./configure --enable-netcdf-4 --enable-shared --enable-dap --prefix=$ifolder_ncC #1>/dev/null
+
+make #1>/dev/null
+make check #1>/dev/null
+sudo make install
+cd $og_path
+
+
+################# netcdf-F #################
+
+echo "Downloading netcdf-F version ${ncFv}"
+echo "Downloading netcdf-F version ${ncFv}" >> installation_log.txt
+wget https://github.com/Unidata/netcdf-fortran/archive/v${ncFv}.tar.gz
+mv v${ncFv}.tar.gz ncF_v${ncFv}.tar.gz
+tar xvzf ncF_v${ncFv}.tar.gz >/dev/null
+
+cd netcdf-fortran-${ncFv}
+
+echo "Installing netcdf-f version ${ncFv}"
+echo "Installing netcdf-f version ${ncFv}" >> installation_log.txt
+
+CPPFLAGS=-I$ifolder_ncC/include LDFLAGS=-L$ifolder_ncC/lib ./configure --enable-netcdf-4 --enable-shared --enable-dap --prefix=$ifolder_ncF #1>/dev/null
+make #1>/dev/null
+make check #1>/dev/null
+sudo make install
+
+cd $og_path
+
+echo "Installation complete!" >> installation_log.txt
+echo "Installation complete!"
+
+# General comments:
+# --enable-hl          Enables high level library support, for libraries such as netcdf4-python.
+# --enable-shared	     Will (attempt to) build shared libraries on your system.
+# --enable-netcdf-4	   Builds netCDF so that netCDF-4/HDF5 files may be produced. 
+#                      NetCDF-4/HDF5 files are necessary to use any of the new 
+#                      features of netCDF-4, including compression, parallel I/O, 
+#                      and the enhanced data model. Without this option (that is, 
+#                      by default) you do not get the netCDF-4/HDF5 features.
+# --enable-dap         Built with OPenDAP support.
